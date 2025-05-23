0dfd87cc4016dc7df3c5f540dcfbca3c1585921b
RenanFMarins <renan.fmarins@gmail.com>
2025-05-23 16:09:02 -0300

    spider walks to the character

---
+++ Assets/SpiderAI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderAI : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+            agent.SetDestination(target.position);
+    }
+}
+
